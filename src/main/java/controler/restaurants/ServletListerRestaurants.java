bab736447c7d2955b1883cef699792e4bf6819f8
Madina <madinaodou@gmail.com>
2024-02-23 16:54:31 +0100

    Modif ServletListerRestaurants

--- src/main/java/controler/restaurants/ServletListerRestaurants.java
+++ src/main/java/controler/restaurants/ServletListerRestaurants.java
@@ -28,30 +28,30 @@ public class ServletListerRestaurants extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		try {
-
-
-			List<Restaurant> restaurants = restaurantBLL.selectAll();
-			
-			HoraireParJour hpj = new HoraireParJour();
-			hpj.ajouterHoraire("Lundi", new Horaire("10h00", "20h00"));
-			hpj.ajouterHoraire("Mardi", new Horaire("10h00", "20h00"));
-			hpj.ajouterHoraire("Mercredi", new Horaire("10h00", "20h00"));
-			hpj.ajouterHoraire("Jeudi", new Horaire("10h00", "20h00"));
-			hpj.ajouterHoraire("Vendredi", new Horaire("10h00", "23h00"));
-			hpj.ajouterHoraire("Samedi", new Horaire("10h00", "23h00"));
-			hpj.ajouterHoraire("Dimanche", new Horaire("10h00", "23h00"));
-			
-			request.setAttribute("horaires", hpj);
-			
-			request.setAttribute("restaurants", restaurants);
-			 
-
-		} catch (BLLException e) {
-			e.printStackTrace();
-		}
-		
-		request.getRequestDispatcher("/WEB-INF/jsp/restaurants.jsp").forward(request, response);
+//		try {
+//
+//
+//			List<Restaurant> restaurants = restaurantBLL.selectAll();
+//			
+//			HoraireParJour hpj = new HoraireParJour();
+//			hpj.ajouterHoraire("Lundi", new Horaire("10h00", "20h00"));
+//			hpj.ajouterHoraire("Mardi", new Horaire("10h00", "20h00"));
+//			hpj.ajouterHoraire("Mercredi", new Horaire("10h00", "20h00"));
+//			hpj.ajouterHoraire("Jeudi", new Horaire("10h00", "20h00"));
+//			hpj.ajouterHoraire("Vendredi", new Horaire("10h00", "23h00"));
+//			hpj.ajouterHoraire("Samedi", new Horaire("10h00", "23h00"));
+//			hpj.ajouterHoraire("Dimanche", new Horaire("10h00", "23h00"));
+//			
+//			request.setAttribute("horaires", hpj);
+//			
+//			request.setAttribute("restaurants", restaurants);
+//			 
+//
+//		} catch (BLLException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		request.getRequestDispatcher("/WEB-INF/jsp/restaurants.jsp").forward(request, response);
 	}
 
 }
